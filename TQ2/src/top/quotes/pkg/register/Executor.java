6f4140a916056f802aa175612556d321ad0d7ee9
maxml <maxml@inbox.ru>
2013-12-06 23:37:01 +0200

    exec

---
+++ TQ2/src/top/quotes/pkg/register/Executor.java
@@ -0,0 +1,30 @@
+package top.quotes.pkg.register;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.protocol.HTTP;
+
+import com.google.gson.Gson;
+
+import top.quotes.pkg.entity.User;
+
+public class Executor {
+
+	private Gson gson = new Gson();
+	private final static String url = "";
+
+	public void sendUSerInfo(User user) {
+		try {
+			HttpPost httpPost = new HttpPost(url);
+			StringEntity buff = new StringEntity(gson.toJson(user).toString(), HTTP.UTF_8);
+			httpPost.setHeader("Accept", "application/json");
+			httpPost.setHeader("Content-type", "application/json");
+			httpPost.setEntity(buff);
+
+			new AsyncRequest().execute(httpPost);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
